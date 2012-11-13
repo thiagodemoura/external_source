86d2aabfd4a7394d349de29aa810dcd627a3a6ae
Rodrigo Oliveira <rodrigo_oliveira@atlantico.com.br>
2012-11-13 10:45:40 -0300

    Refactoring

--- ItextSharpC/Template/Provider/ExcelDataProvider.cs
+++ ItextSharpC/Template/Provider/ExcelDataProvider.cs
@@ -5,6 +5,9 @@ namespace Com.Hp.SRA.Proofing.Chart.Template.Provider
 {
     public class ExcelDataProvider : IDataProvider
     {
+        private const string XlsxExtension = ".xlsx";
+        private const string XlsExtension = ".xls";
+
         /// <summary>
         /// Gets or sets the data source.
         /// </summary>
@@ -30,12 +33,12 @@ namespace Com.Hp.SRA.Proofing.Chart.Template.Provider
               System.IO.FileStream stream = System.IO.File.Open(DataSource, System.IO.FileMode.Open, System.IO.FileAccess.Read);
 
               IExcelDataReader excelReader = null;
-              if (DataSource.EndsWith(".xls"))
+              if (IsXlsDataSource())
               {
                   //Reading from a binary Excel file ('97-2003 format; *.xls)
                   excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
               }
-              else if (DataSource.EndsWith(".xlsx"))
+              else if (IsXlsxDataSource())
               {
                   //Reading from a  Excel file ('2007 format; *.xlsx)
                   excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
@@ -43,5 +46,15 @@ namespace Com.Hp.SRA.Proofing.Chart.Template.Provider
               stream.Close();
               return excelReader;
           }
+
+        private bool IsXlsxDataSource()
+        {
+            return DataSource.EndsWith(XlsxExtension);
+        }
+
+        private bool IsXlsDataSource()
+        {
+            return DataSource.EndsWith(XlsExtension);
+        }
     }
 }
